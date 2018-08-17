0bfeb40fdb0b4c3b07ee8d4a0489574a06f0c8d6
pavlofilchuk <p.filchuk@gmail.com>
2018-08-17 15:33:45 +0300

    Merge fix

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,17 +1,10 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-<<<<<<< HEAD
 
 import Homepage from './homepage/Homepage';
-
-//Router and components
-
-export default class Main extends React.Component<{}, {}>{
-    render() {
-        return <div><Homepage /></div> // <Pavlo component> <>
-=======
 import { createStore } from "./store"
 import { Provider } from "react-redux";
+//Router and components
 
 export default class Main extends React.Component<{}, {}>{
     render() {
@@ -19,8 +12,7 @@ export default class Main extends React.Component<{}, {}>{
         let store = createStore();
 
         return <Provider store={store}>
-            <div>Test</div>
+            <Homepage />
         </Provider>
->>>>>>> 8779620a8f8c2a086622e1eb7cc83a99ca20f30d
     }
 }
