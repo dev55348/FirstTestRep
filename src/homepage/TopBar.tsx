896659004fd3b1dc5e7cd4b379d9485fcf5cb06e
copidaster <andriy.zaluha@yahoo.com>
2018-08-17 17:41:42 +0200

    Commit.

--- src/homepage/TopBar.tsx
+++ src/homepage/TopBar.tsx
@@ -1,24 +1,26 @@
 import * as React from 'react';
 
 import Localization from './Localization';
+import { withLocalize,LocalizeContextProps } from 'react-localize-redux';
 
-class TopBar extends React.Component<{}, {}>{
-    render(){
-      return <header className="header">
-        <button className="btn-menu"><span></span><span></span><span></span></button>
-        <ul className="top-bar top-left-bar">
-            <li><a className="logo" href='#'>KnowpiaLogo</a></li>
-            <li><a href='#'>KNOWPedia</a></li>
-            <li><a href='#'>Blog</a></li>
-        </ul>
-        <ul className="top-bar top-right-bar">
-            <li><a href="#">How it works</a></li>
-            <li><a href="#">Sign in</a> | <a href="#">Get Started</a></li>
-            <li>
-                <Localization />
-            </li>
-        </ul>
-      </header>
+
+class TopBar extends React.Component<any, {}>{
+    render() {
+        return <header className="header">
+            <button className="btn-menu"><span></span><span></span><span></span></button>
+            <ul className="top-bar top-left-bar">
+                <li><a className="logo" href='#'>KnowpiaLogo</a></li>
+                <li><a href='#'>KNOWPedia</a></li>
+                <li><a href='#'>Blog</a></li>
+            </ul>
+            <ul className="top-bar top-right-bar">
+                <li><a href="#">{this.props.translate("topbar.howItWorks")}</a></li>
+                <li><a href="#">Sign in</a> | <a href="#">Get Started</a></li>
+                <li>
+                    <Localization />
+                </li>
+            </ul>
+        </header>
     }
 }
-export default TopBar
+export default withLocalize<any>(TopBar)
