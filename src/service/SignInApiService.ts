73d2090c4c5659a37c6c4da46628c99623032947
andrey <>
2018-08-29 12:39:10 +0300

    updateToken

--- src/service/SignInApiService.ts
+++ src/service/SignInApiService.ts
@@ -11,7 +11,7 @@ export default class SignInApiService {
     public static sendRequest(signInData: SignIn,callback: (items: string) => void) {
 
         var items: string = '';
-        var requestUrl = "http://test.api.en.knowpia.cn/user/sign?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1MzU0NTI4MDcsImlhdCI6MTUzNTQ0NTYwNywia2V5IjoiRU42NDAwNTkyIiwic2lkIjoiaGhmcTlvamRvNHJ1ZW0yNXVkM2VuMGFuYjUifQ.oMziOwuZZaVqBtLfgjtZLRoTn95LTn-JwZjO7jjEVfE";
+        var requestUrl = "http://test.api.en.knowpia.cn/user/sign?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1MzU1NDIwODUsImlhdCI6MTUzNTUzNDg4NSwia2V5IjoiRU42NDAwNTkyIiwic2lkIjoiaGhmcTlvamRvNHJ1ZW0yNXVkM2VuMGFuYjUifQ.a7aue43DKO2mQ7swyorVd5KH26RfJMSjXh_fxb_lHAg";
         debugger;
         const params = new URLSearchParams();
         params.append('account', 'testAPIaccount');
